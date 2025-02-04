5ccca9a4414d48be2687aa59de44a4569361392c
Abdillamit Mamytov <abdillamit@mancho.dev>
2025-02-04 23:50:05 +0600

    Fix navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3">
       <Link to={"/"}>
-        <img src={logo} width={50} height={50} alt="" />
+        <img src={logo} width={50} height={50} />
       </Link>
 
       <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
